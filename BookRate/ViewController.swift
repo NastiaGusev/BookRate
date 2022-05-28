abe7517fe24dc07a4bd4eb1f8ca967c76421d193
Nastia Gusev <gusev.nastia@gmail.com>
2022-05-28 12:05:27 +0300

    Initial Commit

---
+++ BookRate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookRate
+//
+//  Created by Nastia Gusev on 28/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
